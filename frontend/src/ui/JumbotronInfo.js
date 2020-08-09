5b483d68ac561d3aed7d502dbfd8fef0463728aa
DylanDeraad <67024912+DylanDeraad@users.noreply.github.com>
2020-08-09 12:48:23 -0600

    got jumbo tron for info page working and good

--- frontend/src/ui/JumbotronInfo.js
+++ frontend/src/ui/JumbotronInfo.js
@@ -1,31 +1,41 @@
 import React from "react";
 import Jumbotron from "react-bootstrap/cjs/Jumbotron";
 import {Container, Row, Col} from 'reactstrap';
+import Image from 'react-bootstrap/Image';
 
 export const JumbotronInfo = (props) => {
   return (
     <>
-      <Jumbotron>
-        <Container>
-          <Row>
-            <Col xs="6"></Col>
-            <Col xs="6">
-              <h1>plant name</h1>
-              <ul>
-                <li>State:</li>
-                <li>Family Name:</li>
-                <li>Common Name:</li>
-                <li>Scientific Name:</li>
-                <li>Color:</li>
-                <li>Duration:</li>
-                <li>Light Preference:</li>
-                <li>Size:</li>
-              </ul>
-            </Col>
-          </Row>
-        </Container>
+      <Container>
+        <Row>
+          <Col xs={{ size: 12}}>
+            <Jumbotron fluid>
+              <Container>
+                <Row>
+                  <Col  md="6">
+                    <Image src="React-Static-Example.jpg" fluid />
+                  </Col>
+                  <Col md="6">
+                    <h1>plant name</h1>
+                    <ul>
+                      <li>State:</li>
+                      <li>Family Name:</li>
+                      <li>Common Name:</li>
+                      <li>Scientific Name:</li>
+                      <li>Color:</li>
+                      <li>Duration:</li>
+                      <li>Light Preference:</li>
+                      <li>Size:</li>
+                    </ul>
+                  </Col>
+                </Row>
+              </Container>
 
-      </Jumbotron>
+            </Jumbotron>
+          </Col>
+        </Row>
+      </Container>
+
     </>
   )
 }
