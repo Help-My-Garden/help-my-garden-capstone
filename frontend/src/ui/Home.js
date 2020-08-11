caaccb8065bdda2f8a677f85d9533d1f0866218b
James Mathieus Jr <jlmathieus@gmail.com>
2020-08-10 18:58:27 -0600

    changed home com function to proper syntax

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -2,10 +2,10 @@ import React from "react"
 import 'bootstrap/dist/css/bootstrap.css';
 import {NavbarCom} from "./Navbar";
 
-export const Home = () => (
-
-	<>
-		<NavbarCom></NavbarCom>
-	</>
-
-)
+export const Home = () => {
+	return (
+		<>
+			<NavbarCom/>
+		</>
+	)
+}
