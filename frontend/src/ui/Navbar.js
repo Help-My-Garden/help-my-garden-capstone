0a951fbb1a817cbf4ff6467288bc7a1972c47fa8
James Mathieus Jr <jlmathieus@gmail.com>
2020-08-10 18:40:01 -0600

    added logo and linkd for landing page

--- frontend/src/ui/Navbar.js
+++ frontend/src/ui/Navbar.js
@@ -2,24 +2,22 @@ import React from "react"
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Nav from "react-bootstrap/Nav";
+import image from "./shared/world-logo-black.png"
 
 export const NavbarCom = () => {
 
 	return (
 		<>
 			<Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-				<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+				<img src = {image} alt = "Help My Garden logo" width="100"/>
+				<Navbar.Brand href="#home">Help My Garden</Navbar.Brand>
 				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 				<Navbar.Collapse id="responsive-navbar-nav">
 					<Nav className="ml-auto">
-						<Nav.Link href="#features">Features</Nav.Link>
-						<Nav.Link href="#pricing">Pricing</Nav.Link>
-						<NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-							<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-							<NavDropdown.Divider />
-							<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+						<Nav.Link href="#features">Search</Nav.Link>
+						<Nav.Link href="#pricing">Creat Account</Nav.Link>
+						<NavDropdown title="Account" id="collasible-nav-dropdown">
+							<NavDropdown.Item href="#action/3.1">Log In</NavDropdown.Item>
 						</NavDropdown>
 					</Nav>
 				</Navbar.Collapse>
