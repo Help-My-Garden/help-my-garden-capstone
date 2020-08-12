37ef587ef38d7faec74645d5ddd28168839004ae
James Mathieus Jr <jlmathieus@gmail.com>
2020-08-11 22:46:39 -0600

    added 2 modals to navbar and they are working!

--- frontend/src/ui/Navbar.js
+++ frontend/src/ui/Navbar.js
@@ -2,9 +2,23 @@ import React from "react"
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Nav from "react-bootstrap/Nav";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import image from "./shared/world-logo-black.png"
+import {useEffect, useState} from "react";
+
+
 
 export function NavbarCom () {
+
+	/*added different useStates for the different modals*/
+	const [show, setShow] = useState(false);
+	const [show2, setShow2] = useState(false)
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+	const handleClose2 = () => setShow2(false);
+	const handleShow2 = () => setShow2(true);
 
 	return (
 		<>
@@ -15,13 +29,49 @@ export function NavbarCom () {
 				<Navbar.Collapse id="responsive-navbar-nav">
 					<Nav className="ml-auto">
 						<Nav.Link href="#features">Search</Nav.Link>
-						<Nav.Link href="#pricing">Creat Account</Nav.Link>
-						<NavDropdown title="Account" id="collasible-nav-dropdown">
-							<NavDropdown.Item href="#action/3.1">Log In</NavDropdown.Item>
+						<Nav.Link href="#pricing"onClick={handleShow2}>Creat Account</Nav.Link>
+						<NavDropdown title="Account" id="collapsible-nav-dropdown">
+							<NavDropdown.Item href="#action/3.1" onClick={handleShow}>Log In</NavDropdown.Item>
 						</NavDropdown>
 					</Nav>
 				</Navbar.Collapse>
 			</Navbar>
+
+			{/*log in modal*/}
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Modal heading</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					I'm modal number 1!
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+						Close
+					</Button>
+					<Button variant="primary" onClick={handleClose}>
+						Save Changes
+					</Button>
+				</Modal.Footer>
+			</Modal>
+
+			{/*creat account  modal*/}
+			<Modal show={show2} onHide={handleClose2}>
+				<Modal.Header closeButton>
+					<Modal.Title>Modal heading</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					I'm modal number 2!
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose2}>
+						Close
+					</Button>
+					<Button variant="primary" onClick={handleClose2}>
+						Save Changes
+					</Button>
+				</Modal.Footer>
+			</Modal>
 		</>
 	)
 }
