43b9b1781e4bce876403707dc1b6f54963b16df4
James Mathieus Jr <jlmathieus@gmail.com>
2020-08-12 16:03:51 -0600

    changed cpmonents around

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,25 +5,11 @@ import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
 import {FourOhFour} from "./ui/FourOhFour";
 import {Home} from "./ui/Home";
-import {NavbarCom} from "./ui/Navbar";
-import {JumbotronCom} from "./ui/Jumbotron";
-import {CarouselCom} from "./ui/Carousel";
-import {Footer} from "./ui/Footer";
 
 
 const Routing = () => (
 	<>
 		<BrowserRouter>
-			<div style={{
-			display: "block",
-			margin: "2rem 4rem 2rem 4rem",
-			backgroundColor: "grey",
-		}}>
-			<NavbarCom/>
-			<JumbotronCom/>
-			<CarouselCom/>
-			<Footer/>
-		</div>
 			<Switch>
 				<Route exact path="/" component={Home}/>
 				<Route component={FourOhFour}/>
--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,9 +1,23 @@
 import React from "react"
 import 'bootstrap/dist/css/bootstrap.css';
+import {JumbotronCom} from "./Jumbotron";
+import {CarouselCom} from "./Carousel";
+import {NavbarCom} from "./Navbar";
+import {Footer} from "./Footer";
 
 export const Home = () => {
 	return (
 		<>
+			<div style={{
+			display: "block",
+			margin: "2rem 4rem 2rem 4rem",
+			backgroundColor: "grey",
+		}}>
+			<NavbarCom/>
+			<JumbotronCom/>
+			<CarouselCom/>
+			<Footer/>
+		</div>
 			</>
 	)
 }
