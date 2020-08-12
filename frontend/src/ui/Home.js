03db0c9120269c4829451a4db02709e5c7d4b3e1
James Mathieus Jr <jlmathieus@gmail.com>
2020-08-12 15:38:17 -0600

    moved components to index.js

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,23 +1,9 @@
 import React from "react"
 import 'bootstrap/dist/css/bootstrap.css';
-import {NavbarCom} from "./Navbar";
-import {JumbotronCom} from "./Jumbotron";
-import {CarouselCom} from "./Carousel";
-import {Footer} from "./Footer";
 
 export const Home = () => {
 	return (
 		<>
-			<div style={{
-				display: "block",
-				margin: "2rem 4rem 2rem 4rem",
-				backgroundColor: "grey",
-			}}>
-				<NavbarCom/>
-				<JumbotronCom/>
-				<CarouselCom/>
-				<Footer/>
-				</div>
 			</>
 	)
 }
