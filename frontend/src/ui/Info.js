d2c065c1fc6d789b3444c3185bdfe2194a69e9a4
James Mathieus Jr <jlmathieus@gmail.com>
2020-08-14 11:19:50 -0600

    added correct package.json and package-lock.json

--- frontend/src/ui/Info.js
+++ frontend/src/ui/Info.js
@@ -1,11 +1,9 @@
 import React from "react"
 import 'bootstrap/dist/css/bootstrap.css';
-import {NavbarInfo} from "./NavbarInfo"
 import {JumbotronInfo} from "./JumbotronInfo";
 
 export const Info = () => (
   <>
-    <NavbarInfo></NavbarInfo>
     <JumbotronInfo></JumbotronInfo>
   </>
 
