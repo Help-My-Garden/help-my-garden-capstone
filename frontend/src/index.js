fc999a41d29ab67baf1b56726c334759e43931ab
James Mathieus Jr <jlmathieus@gmail.com>
2020-08-14 14:27:16 -0600

    fixing delet issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,18 @@ import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
 import {FourOhFour} from "./ui/FourOhFour";
 import {Home} from "./ui/Home";
+import {SearchPage} from "./ui/SearchPage";
+import {Info} from "./ui/Info";
+import {MyListCom} from "./ui/MyList";
 
 function Routing (){
 	return(
 	<>
 		<BrowserRouter>
 			<Switch>
+				<Route exact path="/search" component={SearchPage}/>
+				<Route exact path="/info" component={Info}/>
+				<Route exact path="/my-list" component={MyListCom}/>
 				<Route exact path="/" component={Home}/>
 				<Route component={FourOhFour}/>
 			</Switch>
---
+++ frontend/src/ui/Info.js
@@ -0,0 +1,30 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.css';
+import {JumbotronInfo} from "../help-my-garden-capstone/frontend/src/ui/JumbotronInfo";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import image from "./shared/world-logo-black.png";
+
+export const Info = () => (
+  <>
+    <Navbar className="row fluid navbar-dark" collapseOnSelect expand="md" style={{
+      backgroundColor: "black",
+    }}>
+      <img src = {image} alt = "Help My Garden logo" width="100"/>
+      <Navbar.Brand style={{color: "white",}}>Help My Garden</Navbar.Brand>
+      <Navbar.Toggle className="ml-auto" aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="/" style={{color: "white",}}>Home</Nav.Link>
+          <Nav.Link href="/search" style={{color: "white",}}>Search</Nav.Link>
+          <NavDropdown title="Account " id="collapsible-nav-dropdown">
+            <NavDropdown.Item href="#log-in-modal">My list</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    <JumbotronInfo></JumbotronInfo>
+  </>
+
+)
