3bc527b298683abcdde31bae369b40d7ba659316
James Mathieus Jr <jlmathieus@gmail.com>
2020-08-14 14:05:51 -0600

    changed home page back to hame

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
 import {FourOhFour} from "./ui/FourOhFour";
-import {MyListCom} from "./ui/MyList";
+import {Home} from "./ui/Home";
 
 function Routing (){
 	return(
 	<>
 		<BrowserRouter>
 			<Switch>
-				<Route exact path="/" component={MyListCom}/>
+				<Route exact path="/" component={Home}/>
 				<Route component={FourOhFour}/>
 			</Switch>
 		</BrowserRouter>
