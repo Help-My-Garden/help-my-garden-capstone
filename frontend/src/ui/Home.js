56f03ee127af29c0e40ba8ac52f6047c01700480
James Mathieus Jr <jlmathieus@gmail.com>
2020-08-14 09:15:35 -0600

    renamed navbar component to universalavbar

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,6 +1,6 @@
 import React from "react"
 import 'bootstrap/dist/css/bootstrap.css';
-import {NavbarCom} from "./Navbar";
+import {NavbarCom} from "./UniversalNavbar";
 
 export const Home = () => {
 	return (
