aaff16fe1a4716f557473f3bf4af257c1cc86541
James Mathieus Jr <jlmathieus@gmail.com>
2020-08-14 13:37:16 -0600

    created MyList compenent added navbar and search bar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,18 +4,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
 import {FourOhFour} from "./ui/FourOhFour";
-import {Home} from "./ui/Home";
-import {SearchPage} from "./ui/SearchPage";
-import {Info} from "./ui/Info";
+import {MyListCom} from "./ui/MyList";
 
 function Routing (){
 	return(
 	<>
 		<BrowserRouter>
 			<Switch>
-				<Route exact path="/info" component={Info}/>
-				<Route exact path="/search" component={SearchPage}/>
-				<Route exact path="/" component={Home}/>
+				<Route exact path="/" component={MyListCom}/>
 				<Route component={FourOhFour}/>
 			</Switch>
 		</BrowserRouter>
---
+++ frontend/src/ui/MyList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {SearchCom} from "./Search";
+import Navbar from "react-bootstrap/Navbar";
+import image from "./shared/world-logo-black.png";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+export const MyListCom = () =>{
+	return(
+		<>
+			<Navbar className="row fluid navbar-dark" collapseOnSelect expand="md" style={{
+				backgroundColor: "black",
+			}}>
+				<img src = {image} alt = "Help My Garden logo" width="100"/>
+				<Navbar.Brand style={{color: "white",}}>Help My Garden</Navbar.Brand>
+				<Navbar.Toggle className="ml-auto" aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="ml-auto">
+						<Nav.Link href="/" style={{color: "white",}}>Home</Nav.Link>
+						<Nav.Link href="/search" style={{color: "white",}}>Search</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		<SearchCom/>
+		<div className="container" style={{
+			backgroundColor: "grey",
+		}}>
+			<h2>I'm the container</h2>
+		</div>
+			</>
+	)
+}
+
