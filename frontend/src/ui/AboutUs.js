1a50c60963005755133a913a16babdc9fae9097f
James Mathieus Jr <jlmathieus@gmail.com>
2020-08-15 13:20:11 -0600

    added content to welcome to help my garden and meet the team with content

--- frontend/src/ui/AboutUs.js
+++ frontend/src/ui/AboutUs.js
@@ -10,16 +10,17 @@ export const AboutUs = (props) => {
         <Row>
           <Col xs={{ size: 12}}>
             <Jumbotron>
-              <h1>About Help My Garden!</h1>
-              <p>this would be about the website Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
-              <p>this would be about James Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
-              <p>about Dylan Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
-
+              <h1>Welcome to Help My Garden</h1>
+              <h3>Search plants across the entire USA!</h3>
+              <p>Help my garden was designed for people who want to build a beautiful garden but aren't sure what plants to grow. This site searches the Trefle database witch has over 18,000 plants in the USA alone. This allows you to search your specific state and the site will pull up a list of all plants that are native to the area. Create an account and save your plants to a custom list you can reference later. With all that being said, 'Welcome To Our Site!' We hope you have as much fun as we did creating it.</p>
+            </Jumbotron>
+            <Jumbotron>
+              <h2>Meet the Team!</h2>
+              <p>We are a team of web developers in New Mexico with a goal. That goal is to make beautiful websites that have reusable easy to understand content. One of the things we dislike is an unusable site that has way to many bells and whistles. Sites should be something easy to use rather than needing to come with an instruction manual. With this vision in mind Help My Garden was born.</p>
             </Jumbotron>
           </Col>
         </Row>
       </Container>
-
     </>
   )
 }
