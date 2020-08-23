1a210bfc3a2badc2ab34ec5f828041ac3495affd
DylanDeraad <67024912+DylanDeraad@users.noreply.github.com>
2020-08-23 10:16:39 -0600

    adding what i believe is nessesary for user signup handeler

--- backend/utils/profile/selectProfileByProfileEmail.ts
+++ backend/utils/profile/selectProfileByProfileEmail.ts
@@ -1,15 +1,14 @@
 import {connect} from "../../src/database";
 import {Profile} from "../interfaces/Profile";
 
-export async function selectProfileByProfileActivationToken(profileActivationToken: string) {
+export async function selectProfileByProfileEmail(profileEmail: string) {
     try {
         const mysqlConnection = await connect();
 
-        const [rows] = await mysqlConnection.execute('SELECT BIN_TO_UUID(profileId) as profileId, profileEmail, profileHash, profileName, profileActivationToken FROM profile WHERE profileActivationToken = :profileActivationToken', {profileActivationToken});
-        console.log(rows)
+        const [rows] = await mysqlConnection.execute('SELECT BIN_TO_UUID(profileId) as profileId, profileEmail, profileHash, profileName, profileActivationToken FROM profile WHERE profileEmail = :profileEmail', {profileEmail});
+
         // @ts-ignore is required so that rows can be interacted with like the array it is
         return rows.length !== 0 ? {...rows[0]} : undefined;
-
     } catch (e) {
         console.error(e)
         return undefined
