65550743cd1273b735afa9d81f36037abd06e98b
DylanDeraad <67024912+DylanDeraad@users.noreply.github.com>
2020-08-25 18:52:33 -0600

    just about done setting up handeler

---
+++ backend/utils/profile/selectProfileByProfileId.ts
@@ -0,0 +1,26 @@
+import {connect} from "../../src/database";
+
+/**
+ * Gets a profile by profile id
+ *
+ * @param {string} profileId - id of the profile to select from mysql
+ * @return {(Array | undefined)} rows - array that contains the profile data found, or undefined if errors occur
+ **/
+export async function selectProfileByProfileId(profileId : string) {
+    try {
+
+        const mysqlConnection = await connect();
+
+        // mysql prepared statement
+        const mySqlQuery = 'SELECT BIN_TO_UUID(profileId) as profileId, profileActivationToken, profileEmail, profileHash, profileUsername FROM profile WHERE profileId = UUID_TO_BIN(:profileId)'
+
+        // return the rows from DB
+        const [rows] =  await mysqlConnection.execute(mySqlQuery, {profileId})
+        // @ts-ignore
+        return {...rows[0]}
+
+    } catch(error) {
+        console.log(error)
+        return undefined
+    }
+}
