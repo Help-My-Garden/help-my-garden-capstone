5449c94ccbbcf41ff00792d05c74cf094733b6d0
DylanDeraad <67024912+DylanDeraad@users.noreply.github.com>
2020-08-26 22:10:54 -0600

    adding email validation

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,10 +1,10 @@
-import { createPool, Pool } from 'mysql2/promise'
-
-require('dotenv').config()
+import { createPool, Pool } from 'mysql2/promise';
+require('dotenv').config();
 
 // Database connection. Can use config from .env file (based on example.env) or the defaults
 
 export async function connect (): Promise<Pool> {
+
     const connection = await createPool({
         host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
@@ -12,6 +12,7 @@ export async function connect (): Promise<Pool> {
         database: process.env.MYSQL_DATABASE,
         connectionLimit: 10,
         namedPlaceholders: true
-    })
-    return connection
+    });
+
+    return connection;
 }
