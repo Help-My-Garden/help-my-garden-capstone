4f183c51622a447a8660bcc451ef85f91fc778a3
DylanDeraad <67024912+DylanDeraad@users.noreply.github.com>
2020-08-26 22:10:54 -0600

    adding email validation

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,6 +1,5 @@
-import { createPool, Pool } from 'mysql2/promise'
-
-require('dotenv').config()
+import { createPool, Pool } from 'mysql2/promise';
+require('dotenv').config();
 
 // Database connection. Can use config from help-my-garden.env file (based on example.env) or the defaults
 
@@ -13,6 +12,7 @@ export async function connect (): Promise<Pool> {
         database: process.env.MYSQL_DATABASE,
         connectionLimit: 10,
         namedPlaceholders: true
-    })
-    return connection
+    });
+
+    return connection;
 }
