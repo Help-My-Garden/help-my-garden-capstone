2812bedb4134ad5dc5b64a7d1365c9e17f0ec038
James Mathieus Jr <jlmathieus@gmail.com>
2020-08-31 20:25:21 -0600

    added to plant.cintroller

--- backend/src/controllers/plant.controller.ts
+++ backend/src/controllers/plant.controller.ts
@@ -1,6 +1,9 @@
 import {Request, Response} from "express";
 import {Status} from "../../utils/interfaces/Status";
 import {selectAllPlants} from "../../utils/plant/selectAllPlants";
+import {selectAllPlantsByColor} from "../../utils/plant/selectAllPlantsByColor";
+import {selectAllPlantsByDuration} from "../../utils/plant/selectAllPlantsByDuration";
+import {selectAllPlantsByCommonName} from "../../utils/plant/selectAllPlantsByCommonName";
 
 export async function getPlantsController(request: Request, response: Response) {
 
@@ -12,5 +15,51 @@ export async function getPlantsController(request: Request, response: Response) 
     } catch(error) {
         console.log(error);
     }
+}
+
+try {
+    const data = await selectAllPlantsByColor()
+    // return the response
+    const status: Status = {status: 200, message: null, data};
+    return response.json(status);
+} catch(error) {
+    console.log(error);
+}
+
+try {
+    const data = await selectAllPlantsByCommonName()
+    // return the response
+    const status: Status = {status: 200, message: null, data};
+    return response.json(status);
+} catch(error) {
+    console.log(error);
+}
+
+try {
+    const data = await selectAllPlantsByDuration()
+    // return the response
+    const status: Status = {status: 200, message: null, data};
+    return response.json(status);
+} catch(error) {
+    console.log(error);
+}
+
+try {
+    const data = await selectAllPlantsByFamilyName()
+    // return the response
+    const status: Status = {status: 200, message: null, data};
+    return response.json(status);
+} catch(error) {
+    console.log(error);
+}
+
+
+try {
+    const data = await selectAllPlantsById()
+    // return the response
+    const status: Status = {status: 200, message: null, data};
+    return response.json(status);
+} catch(error) {
+    console.log(error);
 }
 
