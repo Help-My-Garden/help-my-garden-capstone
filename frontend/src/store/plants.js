e1a45c423a3d304b12f7a718c7d5633a3888668e
DylanDeraad <67024912+DylanDeraad@users.noreply.github.com>
2020-09-04 17:49:47 -0600

    added fetch plant by id and plant detail page

--- frontend/src/store/plants.js
+++ frontend/src/store/plants.js
@@ -7,15 +7,22 @@ const slice = createSlice({
   reducers: {
     getAllPlants: (plants, action) => {
       return action.payload
+    },
+    getPlantByPlantId : (plants, action) => {
+      plants.push(action.payload)
     }
   }
 })
 
-export const {getAllPlants} = slice.actions
+export const {getAllPlants, getPlantByPlantId} = slice.actions
 
 export const fetchAllPlants = () => async (dispatch) => {
   const {data} =  await httpConfig.get("/apis/plants/");
   dispatch(getAllPlants(data));
 };
 
+export const fetchPlantByPlantId = (id) => async (dispatch) => {
+  const {data} =  await httpConfig.get(`/apis/plants/${id}`);
+  dispatch(getPlantByPlantId(data));
+};
 export default slice.reducer
--- frontend/src/ui/plant/Plant.js
+++ frontend/src/ui/plant/Plant.js
@@ -1,33 +1,91 @@
-import React from "react"
-import 'bootstrap/dist/css/bootstrap.css';
-import {JumbotronInfo} from "../shared/components/plant-jumbo/JumbotronInfo";
+import React, { useEffect } from "react"
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import image from "../sharedimg/world-logo-black.png";
+import { useDispatch, useSelector } from 'react-redux'
+import { Col, Container, Row } from 'reactstrap'
+import Jumbotron from 'react-bootstrap/cjs/Jumbotron'
+import Image from 'react-bootstrap/Image'
+import { fetchPlantByPlantId } from '../../store/plants'
 
-export const Plant = () => (
+export const Plant = ({match}) => {
+// Returns the the userPosts store from redux and assigns it to the userPosts variable.
+  const dispatch = useDispatch();
 
-  <>
-    <Navbar className="row fluid navbar-dark" collapseOnSelect expand="md" style={{
-      backgroundColor: "black",
-    }}>
-      <a href = "/" >
-      <img src = {image} alt = "Help My Garden logo" width="100"/>
-      </a >
-      <Navbar.Brand href="/" style={{color: "white",}}>Help My Garden</Navbar.Brand>
-      <Navbar.Toggle className="ml-auto" aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Link href="/" style={{color: "white",}}>Home</Nav.Link>
-          <Nav.Link href="/all-plants" style={{color: "white",}}>Search</Nav.Link>
-          <NavDropdown title="Account " id="collapsible-nav-dropdown">
-            <NavDropdown.Item href="/my-plant-list">My list</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-    <JumbotronInfo/>
-  </>
+  const sideEffects = () => {
+    // The dispatch function takes actions as arguments to make changes to the store/redux.
+    dispatch(fetchPlantByPlantId(match.params.plantId));
 
-)
+  };
+
+// Declare any inputs that will be used by functions that are declared in sideEffects.
+  const sideEffectInputs = [match.params.plantId];
+
+  /**
+   * Pass both sideEffects and sideEffectInputs to useEffect.
+   * useEffect is what handles rerendering of components when sideEffects resolve.
+   * E.g when a network request to an api has completed and there is new data to display on the dom.
+   **/
+  useEffect(sideEffects, sideEffectInputs);
+
+
+  const plant = useSelector(state => (
+    state.plants
+      ? state.plants[0]
+      : null
+  ));
+  console.log(plant)
+  return (
+    <>
+      <Navbar className="row fluid navbar-dark" collapseOnSelect expand="md" style={{
+        backgroundColor: "black",
+      }}>
+        <a href="/">
+          <img src={image} alt="Help My Garden logo" width="100"/>
+        </a>
+        <Navbar.Brand href="/" style={{color: "white",}}>Help My Garden</Navbar.Brand>
+        <Navbar.Toggle className="ml-auto" aria-controls="responsive-navbar-nav"/>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="/" style={{color: "white",}}>Home</Nav.Link>
+            <Nav.Link href="/all-plants" style={{color: "white",}}>Search</Nav.Link>
+            <NavDropdown title="Account " id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="/my-plant-list">My list</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      {/*<JumbotronInfo/>*/}
+      <Container>
+        <Row>
+          <Col xs={{ size: 12}}>
+            <Jumbotron fluid>
+              <Container>
+                <Row>
+                  <Col  md="6">
+                    <Image src="React-Static-Example.jpg" fluid />
+                  </Col>
+                  <Col md="6">
+                    <h1>{plant && plant.plantCommonName}</h1>
+                    <ul>
+                      <li>State:</li>
+                      <li>Family Name:</li>
+                      <li>Common Name:</li>
+                      <li>Scientific Name:</li>
+                      <li>Color:</li>
+                      <li>Duration:</li>
+                      <li>Light Preference:</li>
+                      <li>Size:</li>
+                    </ul>
+                  </Col>
+                </Row>
+              </Container>
+
+            </Jumbotron>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
