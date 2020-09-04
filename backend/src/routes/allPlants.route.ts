28c8627648ef3399d5518ac81110f09bacb259f9
DylanDeraad <67024912+DylanDeraad@users.noreply.github.com>
2020-09-04 15:57:28 -0600

    plant misspelling

--- backend/src/routes/allPlants.route.ts
+++ backend/src/routes/allPlants.route.ts
@@ -21,7 +21,7 @@ allPlantsRouter.route('/plant-color/:plantColor').get(asyncValidatorController([
 
 allPlantsRouter.route('/plant-common-name/:plantCommonName').get(asyncValidatorController([check("plantCommonName", "please provide plant common name").isString().notEmpty().trim().escape()]), getPlantsByCommonNameController)
 
-allPlantsRouter.route('/plant-duration/:platDuration').get(asyncValidatorController([check("plantDuration", "please provide plant duration").isString().notEmpty().trim().escape()]), getPlantsByDurationController)
+allPlantsRouter.route('/plant-duration/:plantDuration').get(asyncValidatorController([check("plantDuration", "please provide plant duration").isString().notEmpty().trim().escape()]), getPlantsByDurationController)
 
 allPlantsRouter.route('/plant-family-name/:plantFamilyName').get(asyncValidatorController([check("plantFamilyName", "please provide plant family name").isString().notEmpty().trim().escape()]), getPlantsByFamilyNameController)
 
