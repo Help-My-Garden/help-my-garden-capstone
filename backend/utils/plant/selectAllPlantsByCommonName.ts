f14a7e1b621d036731629ae2d267e2a4c92f8092
DylanDeraad <67024912+DylanDeraad@users.noreply.github.com>
2020-09-08 15:57:10 -0600

    adding a search component to my sql statments

--- backend/utils/plant/selectAllPlantsByCommonName.ts
+++ backend/utils/plant/selectAllPlantsByCommonName.ts
@@ -4,7 +4,7 @@ export async function selectAllPlantsByCommonName(plantCommonName: string) {
     try {
         const mysqlConnection = await connect();
 
-        const [rows] = await mysqlConnection.execute('SELECT BIN_TO_UUID(plantId) as plantId, plantColor, plantCommonName, plantDuration, plantFamilyName, plantImageUrl, plantScientificName, plantSize, plantSunlight FROM plant WHERE plantCommonName = :plantCommonName LIMIT 0, 25', {plantCommonName});
+        const [rows] = await mysqlConnection.execute('SELECT BIN_TO_UUID(plantId) as plantId, plantColor, plantCommonName, plantDuration, plantFamilyName, plantImageUrl, plantScientificName, plantSize, plantSunlight FROM plant WHERE plantCommonName LIKE :plantCommonName  ORDER BY plantCommonName asc LIMIT 200', {plantCommonName: `%${plantCommonName}%`});
         // @ts-ignore is required so that rows can be interacted with like the array it is
         return rows
 
--- frontend/src/ui/shared/components/plant-catagory-selector/Search.js
+++ frontend/src/ui/shared/components/plant-catagory-selector/Search.js
@@ -12,35 +12,12 @@ export function SearchCom () {
 			}}>
 				<div className="row p-2">
 					<div className = "information col-md-2" >
-						<label className = "form-label" >State
-							<input className = "form-control col-md-12" type = "text" name = "State" id = "State"
-										 placeholder = "State" />
-						</label >
-					</div >
-					<div className = "information col-md-2" >
-						<label className = "form-label" >Plant Family
-							<input className = "form-control col-md-12" type = "text" name = "plantFamily" id = "plantFamily"
-										 placeholder = "Plant Family" />
-						</label >
-					</div >
-					<div className = "information col-md-2" >
 						<label className = "form-label" >Plant Name
 							<input className = "form-control col-md-12" type = "text" name = "plantName" id = "plantName"
 										 placeholder = "Plant Name" />
 						</label >
 					</div >
-					<div className = "information col-md-2" >
-						<label className = "form-label" >Duration
-							<input className = "form-control col-md-12" type = "text" name = "duration" id = "duration"
-										 placeholder = "Duration" />
-						</label >
-					</div >
-					<div className = "information col-md-2" >
-						<label className = "form-label" >Color
-							<input className = "form-control col-md-12" type = "text" name = "color" id = "color"
-										 placeholder = "Color" />
-						</label >
-					</div >
+
 					<div className="col-12 p-2">
 						<Button>Search</Button>
 					</div>
